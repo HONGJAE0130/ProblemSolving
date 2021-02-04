c62f7d55f5f2ea0afc37c1fbfcc9a7d8809412c8
HONGJAE0130 <ghwo998@naver.com>
2021-02-04 14:35:09 +0900

    프로그래머스/heap

---
+++ 더맵게.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+	int answer = 0;
+	priority_queue<int, vector<int>, greater<int>> pq;
+	for (int i = 0; i < scoville.size(); i++) {
+		pq.push(scoville[i]);
+	}
+	int a;
+	int first, second;
+	while (pq.top() < K) {
+		if (pq.size() <= 1) {
+			return -1;
+		}
+		first = pq.top();
+		pq.pop();
+		second = pq.top();
+		pq.pop();
+		a = first + second * 2;
+		pq.push(a);
+		answer++;
+
+	}
+	return answer;
+}
