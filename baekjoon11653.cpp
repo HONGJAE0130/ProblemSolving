988fdb21f571bf9dd930534ef69182c4c3355fdf
HONGJAE0130 <ghwo998@naver.com>
2021-03-20 23:53:48 +0900

    baekjoon11653

---
+++ baekjoon11653.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 2; n > 1; ) {
+		if (n%i == 0) {
+			cout << i << "\n";
+			n = n / i;
+		}
+		else i++;
+	}
+}
