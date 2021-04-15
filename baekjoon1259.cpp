c9feadb60f9054cc6469d81e7688dbb5d8978281
HONGJAE0130 <ghwo998@naver.com>
2021-04-15 22:07:42 +0900

    baekjoon1259

---
+++ baekjoon1259.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string input;
+	while (input != "0") {
+		cin >> input;
+		string buf = input; //reverse 하기 전 값을 저장 
+		reverse(input.begin(), input.end()); // input문자열을 reverse 
+		if (input != "0") {
+			if (input == buf) cout << "yes\n";
+			else cout << "no\n";
+		}
+	}
+
+	return 0;
+}
