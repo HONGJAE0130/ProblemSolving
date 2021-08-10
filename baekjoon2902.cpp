e0409b9ebb49edecc9af1b65b4a27916baa1419e
HONGJAE0130 <ghwo998@naver.com>
2021-08-10 23:41:46 +0900

    baekjoon2902

---
+++ baekjoon2902.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	string answer;
+	cin >> s;
+	for (int i = 0; i < s.length(); i++)
+	{
+		if ('A' <= s[i] && s[i] <= 'Z')
+			answer += s[i];
+	}
+	cout << answer;
+	return 0;
+}
