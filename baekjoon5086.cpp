cdbc27e9250821d52f2656265141ac8149773d34
HONGJAE0130 <ghwo998@naver.com>
2021-11-07 23:42:09 +0900

    baekjoon5086/배수와약수

---
+++ baekjoon5086.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int a, b;
+
+int main()
+{
+	while (1)
+	{
+		cin >> a >> b;
+		if (a == 0 && b == 0) break;
+
+		if (a == 0 || b == 0)
+		{
+			cout << "neither" << "\n";
+		}
+		else
+		{
+			if (a % b == 0) cout << "multiple" << "\n";
+			else if (b % a == 0) cout << "factor" << "\n";
+			else cout << "neither" << "\n";
+		}
+
+	}
+}
