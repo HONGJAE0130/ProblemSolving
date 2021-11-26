4282b692747817ddc6d7bf88615e79c7dcad80fa
HONGJAE0130 <ghwo998@naver.com>
2021-11-26 22:40:18 +0900

    백준4999/아!

---
+++ baekjoon4999.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+	string a, b;
+	cin >> a >> b;
+	int a_size = a.size();
+	int b_size = b.size();
+	a_size >= b_size ? cout << "go" : cout << "no";
+}
