7b12490c8539ab17654476c48436c16b96d69dd8
HONGJAE0130 <ghwo998@naver.com>
2021-12-04 23:49:09 +0900

    오각형/백준

---
+++ baekjoon1964.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long N, sum = 5;
+	cin >> N;
+
+	if (N == 1) cout << sum;
+	else {
+		for (int i = 2; i <= N; i++) {
+			sum += 3 * i + 1;
+		}
+		cout << sum % 45678;
+	}
+
+	return 0;
+}
