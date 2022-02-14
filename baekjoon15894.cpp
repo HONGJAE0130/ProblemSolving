1aec23ffc0cd33cc3aaf7beeb3132d360a3d1b9e
HONGJAE0130 <ghwo998@naver.com>
2022-02-14 23:30:07 +0900

    수학은쳬육과목입니다/백준15894

---
+++ baekjoon15894.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	long long N;		//이렇게 쉬운 문제를 틀렸는지 찾아보니까 범위가 10^9 이였다. 얼른 unsigned int 아니면 long long 으로 바꿔줬다.
+	cin >> N;
+	cout << N * 4;
+}
