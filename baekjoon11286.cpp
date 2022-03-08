04a31843c9665b71ab2b7ae8c3d4307678e1f4d5
HONGJAE0130 <ghwo998@naver.com>
2022-03-09 04:07:06 +0900

    백준/11286/compare

--- baekjoon11286.cpp
+++ baekjoon11286.cpp
@@ -1,31 +1,39 @@
 #include <iostream>
 #include <queue>
-#include <vector>
-#include <map>
+#include <cmath>
 using namespace std;
-
+//연산자 오버라이딩 operator()
+struct compare {
+	bool operator()(int a, int b) {
+		if (abs(a) > abs(b)) {
+			return true;
+		}
+		else if (abs(a) < abs(b)) {
+			return false;
+		}
+		else return a > b;
+	}
+};
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
+	priority_queue<int, vector<int>, compare()> pq;
+	 
 	int N;
 	cin >> N;
 
-	priority_queue<pair<int, int>> q; //우선순위 큐 헤더파일 제공
-	//for(int i=0;i<N;i++)
-	while (N--)
-	{
-		int x;
-		cin >> x;
-		if (x == 0) {
-			if (q.empty()) cout << "0\n"; // 비어있으면 0출력            
+	for (int i = 0; i < N; i++) {
+		int X;
+		cin >> X;
+		if (X == 0) {
+			if (pq.empty()) cout << "0\n";
 			else {
-				cout << -q.top().second << "\n"; // top출력
-				q.pop(); // 출력 후 제거
+				cout << pq.top() << "\n";
+				pq.pop();
 			}
 		}
-		else {       //x != 0 이면
-			q.push({ -abs(x),-x });   //숫자 삽입
+		else {
+			pq.push(X);
 		}
 	}
-	return 0;
 }
