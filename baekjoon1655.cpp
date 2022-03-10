45322dd89df5679ccb487a66c8cfb19e0988574b
HONGJAE0130 <ghwo998@naver.com>
2022-03-10 23:38:39 +0900

    백준1655/우선순위큐/가운데를말해요

--- baekjoon1655.cpp
+++ baekjoon1655.cpp
@@ -1,47 +1,57 @@
-//전위 중위 후위 표기식은 스택관련 문제에서 대표격인 것으로 알고있다.
-//스택을 이용해서 풀어봤다.
 #include <iostream>
-#include <stack>
-#include <string>
+#include <vector>
+#include <queue>
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	string s;
-	stack<char> sta;
-	cin >> s;
-	int ssize = s.size();
-	for (int i = 0; i < ssize; i++) {
-		if (s[i] >= 'A' && s[i] <= 'Z') cout << s[i];
-		else {
-			if (s[i] == ')') {
-				while (sta.top() != '(') {
-					cout << sta.top();
-					sta.pop();
-				}
-				sta.pop();
-			}
-			else if (s[i] == '*' || s[i] == '/') {
-				while (!sta.empty() && (sta.top() == '*' || sta.top() == '/')) {
-					cout << sta.top();
-					sta.pop();
-				}
-				sta.push(s[i]);
-			}
-			else if (s[i] == '+' || s[i] == '-') {
-				while (!sta.empty() && sta.top() != '(') {
-					cout << sta.top();
-					sta.pop();
-				}
-				sta.push(s[i]);
-			}
-			else if (s[i] == '(') sta.push(s[i]);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	/*int cen = 10001;
+	vector<int> v;
+	int T;
+	cin>>T;
+	for(int i=0;i<T;i++){
+		int num;
+		cin>>num;
+		if( i%2==1 ){
+			if( num  == cen ) cout<<cen<<'\n';
+			else if ( num < cen){ cout<< num<<'\n'; cen = num;}
+			else if ( num > cen){ cout<< cen<<'\n'; }
+		}
+		else if(i%2==0 ){
+			if( num  == cen ) cout<<cen<<'\n';
+			else if ( num < cen){ cout<< cen<<'\n'; }
+			else if ( num > cen){ cout<< num<<'\n'; cen = num;}
 		}
 
-	}
-	while (!sta.empty()) {
-		cout << sta.top();
-		sta.pop();
+	}*/
+	int t;
+	cin >> t;
+	priority_queue<int> max; //작은것들의 max값이 top
+	priority_queue<int, vector<int>, greater<int>> min; //큰것들의 min값이 top
+
+	while (t--) {
+		int a, size;
+		cin >> a;
+		if (max.size() == min.size()) {        //이미 짝수 일시 max 에 넣기
+			max.push(a);
+		}
+		else {                                 //홀수 면 min 에 넣기
+			min.push(a);
+		}
+		//두 max.top 과 min의 top을 바꿔주기
+		if (!max.empty() && !min.empty() && max.top() > min.top()) {
+			int max_val, min_val;
+			max_val = max.top();
+			min_val = min.top();
+			max.pop();
+			min.pop();
+			max.push(min_val);
+			min.push(max_val);
+
+		}
+		cout << max.top() << '\n';
 	}
 }
